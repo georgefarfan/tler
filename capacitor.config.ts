6490b5e167f1b9e453f9c9267e7cf46497cde730
Jorge Farfan Coaguila <jfarfan-c@k-lagan.com>
2023-11-23 09:09:16 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tler',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
